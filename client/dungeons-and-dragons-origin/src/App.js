da482aa454accf9b454e3a327a4d2dc8e6077fbb
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-08-12 23:44:52 -0400

    initial setup of app

---
+++ client/dungeons-and-dragons-origin/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Dungeons & Dragons: Origin</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
